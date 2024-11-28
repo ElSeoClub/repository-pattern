216660e317a6272ae6e03d09b9c5187c9b07f6c6
ricardo <elseoclub@gmail.com>
2024-11-28 03:10:58 -0600

    test

--- tests/Feature/MakeBaseRepositoryInterfaceCommandTest.php
+++ tests/Feature/MakeBaseRepositoryInterfaceCommandTest.php
@@ -24,27 +24,20 @@ class MakeBaseRepositoryInterfaceCommandTest extends TestCase
     /** @test */
     public function it_creates_base_repository_interface ()
     {
-        // Crea una instancia del Filesystem para interactuar con el sistema de archivos
         $filesystem = new Filesystem();
 
-        // Verifica que el directorio base exista
         $this->assertTrue(is_dir(base_path('app')), 'La ruta base del entorno de Laravel no es correcta.');
 
-        // Crea una instancia del comando a probar
         $command = new MakeBaseRepositoryInterfaceCommand($filesystem);
 
-        // Establece la aplicación de Laravel en el comando
         $command->setLaravel($this->app);
 
-        // Prepara la entrada y salida vacías para el comando
         $input = new ArrayInput([]);
         $output = new NullOutput();
 
-        // Ejecuta el comando
         $command->run($input, $output);
 
-        // Verifica si el archivo fue creado correctamente
-        $targetPath = base_path('app/Repositories/Interfaces/BaseRepositoryInterface.php');
+        $targetPath = base_path('app/Repositories/Interfaces/BaseRepositoryInterfacex.php');
         $this->assertTrue($filesystem->exists($targetPath), 'El archivo BaseRepositoryInterface no fue creado correctamente.');
     }
 }
